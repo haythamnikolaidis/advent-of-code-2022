5f930156a32e9fb40ce000753ac4c807c636bac5
Ivan Rudolph Whitlock Kruger <ivan@tangentsolutions.co.za>
2023-09-19 21:00:40 +0200

    Completed day 5

--- 5/stacks.dart
+++ 5/stacks.dart
@@ -1,14 +1,77 @@
 import '../util/open_file.dart';
+import '../util/stackonator.dart';
 
 Future<int> main() async {
-  final lines = await ReadLinesFromFile('cargo_1.txt');
-  print(part_one(lines));
+  final lines = await ReadLinesFromFile('cargo.txt');
+  print(part_two(lines));
   return 0;
 }
 
 String part_one(List<String> lines) {
   final data = GetStacksAndInstructions(lines);
-  return '${data.$1.length}, ${data.$2.length}';
+
+  final stack_data = data.$1;
+  final stacks = populate_stacks(stack_data);
+
+  final instructions = parse_instructions(data.$2);
+
+  for (final inst in instructions) {
+    var cs = stacks[inst.from].popMany(amount: inst.amount);
+    stacks[inst.to].pushMany(cs);
+  }
+
+  var result = '';
+
+  stacks.forEach((element) => result += element.peak);
+
+  return result;
+}
+
+String part_two(List<String> lines) {
+  final data = GetStacksAndInstructions(lines);
+
+  final stack_data = data.$1;
+  final stacks = populate_stacks(stack_data);
+
+  final instructions = parse_instructions(data.$2);
+
+  for (final inst in instructions) {
+    var cs = stacks[inst.from].popManyInOrder(amount: inst.amount);
+    stacks[inst.to].pushMany(cs);
+  }
+
+  var result = '';
+
+  stacks.forEach((element) => result += element.peak);
+
+  return result;
+}
+
+List<Stack<String>> populate_stacks(List<String> stack_data) {
+  final stack_count = int.parse(stack_data.removeLast().trim().split(' ').last);
+  var stacks = <Stack<String>>[];
+
+  for (var i = 0; i < stack_count; ++i) {
+    stacks.add(new Stack<String>());
+  }
+
+  stack_data.reversed.forEach((element) {
+    var index = 0;
+    for (var i = 0, len = element.runes.length; i < len; ++i) {
+      if (element.runes.elementAt(i) == '['.codeUnitAt(0)) {
+        //start of itesm
+        final stack_index = i ~/ (element.length / stack_count);
+        stacks[stack_index].push(element.substring(i + 1, i + 2));
+      }
+    }
+  });
+
+  return stacks;
+}
+
+List<Instruction> parse_instructions(List<String> instructions_data) {
+  return List.generate(
+      instructions_data.length, (i) => new Instruction(instructions_data[i]));
 }
 
 (List<String>, List<String>) GetStacksAndInstructions(List<String> lines) {
@@ -22,4 +85,25 @@ String part_one(List<String> lines) {
     lines.sublist(0, seperator),
     lines.sublist(seperator + 1, lines.length)
   );
+}
+
+class Instruction {
+  int amount = 0;
+  int from = 0;
+  int to = 0;
+
+  Instruction(String instructions_datum) {
+    var parts = instructions_datum.split(' ');
+    if (parts[0] != 'move') {
+      this.amount = 0;
+      this.from = 0;
+      this.to = 0;
+    }
+
+    this.amount = int.parse(parts[1]);
+    this.from = int.parse(parts[3]) - 1;
+    this.to = int.parse(parts[5]) - 1;
+  }
+
+  String toString() => 'move $amount from $from to $to';
 }
--- util/stackonator.dart
+++ util/stackonator.dart
@@ -1,14 +1,41 @@
 class Stack<T> {
   final list = <T>[];
-  
+
   void push(T val) => list.add(val);
-  //T pop
-  //popMany
-  //peak
-  //isEmpty
-  //isNotEmpty
 
-  //toString
+  void pushMany(List<T> vals) => vals.forEach(list.add);
 
+  T pop() => list.removeLast();
 
-} 
+  List<T> popMany({int amount = 1}) {
+    var lastItems = list.getRange(list.length - amount, list.length);
+    var result = <T>[];
+
+    for (var i = lastItems.length - 1; i >= 0; --i) {
+      result.add(lastItems.elementAt(i));
+    }
+
+    list.removeRange(list.length - amount, list.length);
+
+    return result;
+  }
+
+  List<T> popManyInOrder({int amount = 1}) {
+    var lastItems = list.getRange(list.length - amount, list.length);
+    var result = <T>[];
+
+    lastItems.forEach(result.add);
+
+    list.removeRange(list.length - amount, list.length);
+
+    return result;
+  }
+
+  T get peak => list.last;
+
+  bool get isEmpty => list.isEmpty;
+
+  bool get isNotEmpty => list.isNotEmpty;
+
+  String toString() => list.toString();
+}
